c66fe89a9858f391f77c96005d7d9bd98cdc3345
Joshua Persechini <joshuapersechini@gmail.com>
2021-04-03 21:56:33 +0100

    Updates to card dealing, character selection, movement, turn order tracking

--- server.js
+++ server.js
@@ -1,3 +1,298 @@
+const E_ROOMS = {
+  STUDY: {
+      name: "Study",
+      type: "room",
+      rightSquare: "hallway1",
+      leftSquare: null,
+      downSquare: "hallway3",
+      upSquare: null,
+      secretPassage: "Kitchen"
+  },
+  HALLWAY1: {
+      name: "hallway1",
+      type: "hallway",
+      rightSquare: "Hall",
+      leftSquare: "Study",
+      downSquare: "hallway4",
+      upSquare: null,
+      secretPassage: null
+  },
+  HALL: {
+      name: "Hall",
+      type: "room",
+      rightSquare: "hallway2",
+      leftSquare: "hallway1",
+      downSquare: "hallway4",
+      upSquare: null,
+      secretPassage: null
+  },
+  HALLWAY2: {
+      name: "hallway2",
+      type: "hallway",
+      rightSquare: "Lounge",
+      leftSquare: "Hall",
+      downSquare: null,
+      upSquare: null,
+      secretPassage: null
+  },
+  LOUNGE: {
+      name: "Lounge",
+      type: "room",
+      rightSquare: null,
+      leftSquare: "hallway2",
+      downSquare: "hallway5",
+      upSquare: null,
+      secretPassage: "Conservatory"
+  },
+  HALLWAY3: {
+      name: "hallway3",
+      type: "hallway",
+      rightSquare: null,
+      leftSquare: null,
+      downSquare: "Library",
+      upSquare: "Study",
+      secretPassage: null
+  },
+  HALLWAY4: {
+      name: "hallway4",
+      type: "hallway",
+      rightSquare: null,
+      leftSquare: null,
+      downSquare: "Billiard Room",
+      upSquare: "Hall",
+      secretPassage: null
+  },
+  HALLWAY5: {
+      name: "hallway5",
+      type: "hallway",
+      rightSquare: null,
+      leftSquare: null,
+      downSquare: "Dining Room",
+      upSquare: "Lounge",
+      secretPassage: null
+  },
+  LIBRARY: {
+      name: "Library",
+      type: "room",
+      rightSquare: "hallway6",
+      leftSquare: null,
+      downSquare: "hallway8",
+      upSquare: "hallway3",
+      secretPassage: null
+  },
+  HALLWAY6: {
+      name: "hallway6",
+      type: "hallway",
+      rightSquare: "Billiard Room",
+      leftSquare: "Library",
+      downSquare: null,
+      upSquare: null,
+      secretPassage: null
+  },
+  BILLIARDROOM: {
+      name: "Billiard Room",
+      type: "room",
+      rightSquare: "hallway7",
+      leftSquare: "hallway6",
+      downSquare: "hallway9",
+      upSquare: "hallway4",
+      secretPassage: null
+  },
+  HALLWAY7: {
+      name: "hallway7",
+      type: "hallway",
+      rightSquare: "Dining Room",
+      leftSquare: "Billiard Room",
+      downSquare: null,
+      upSquare: null,
+      secretPassage: null
+  },
+  DININGROOM: {
+      name: "Dining Room",
+      type: "room",
+      rightSquare: null,
+      leftSquare: "hallway7",
+      downSquare: "hallway10",
+      upSquare: "hallway5",
+      secretPassage: null
+  },
+  HALLWAY8: {
+      name: "hallway8",
+      type: "hallway",
+      rightSquare: null,
+      leftSquare: null,
+      downSquare: "Conservatory",
+      upSquare: "Library",
+      secretPassage: null
+  },
+  HALLWAY9: {
+      name: "hallway9",
+      type: "hallway",
+      rightSquare: null,
+      leftSquare: null,
+      downSquare: "Ballroom",
+      upSquare: "Billiard Room",
+      secretPassage: null
+  },
+  HALLWAY10: {
+      name: "hallway10",
+      type: "hallway",
+      rightSquare: null,
+      leftSquare: null,
+      downSquare: "Kitchen",
+      upSquare: "Dining Room",
+      secretPassage: null
+  },
+  CONSERVATORY: {
+      name: "Conservatory",
+      type: "room",
+      rightSquare: "hallway11",
+      leftSquare: null,
+      downSquare: null,
+      upSquare: "hallway8",
+      secretPassage: "Lounge"
+  },
+  HALLWAY11: {
+      name: "hallway11",
+      type: "hallway",
+      rightSquare: "Ballroom",
+      leftSquare: "Conservatory",
+      downSquare: null,
+      upSquare: null,
+      secretPassage: null
+  },
+  BALLROOM: {
+      name: "Ballroom",
+      type: "room",
+      rightSquare: "hallway12",
+      leftSquare: "hallway11",
+      downSquare: null,
+      upSquare: "hallway9",
+      secretPassage: null
+  },
+  HALLWAY12: {
+      name: "hallway12",
+      type: "hallway",
+      rightSquare: "Kitchen",
+      leftSquare: "Ballroom",
+      downSquare: null,
+      upSquare: null,
+      secretPassage: null
+  },
+  KITCHEN: {
+      name: "Kitchen",
+      type: "room",
+      rightSquare: null,
+      leftSquare: "hallway12",
+      downSquare: null,
+      upSquare: "hallway10",
+      secretPassage: "Study"
+  }
+
+};
+
+const E_WEAPONS = {
+  ROPE: "Rope",
+  PIPE: "Pipe",
+  KNIFE: "Knife",
+  WRENCH: "Wrench",
+  CANDLESTICK: "Candlestick",
+  REVOLVER: "Revolver"
+}
+
+const E_CHARACTERS = {
+  COLONEL: {
+      name: "Colonel Mustard", location: E_ROOMS.HALLWAY5, taken: false
+  },
+  PEACOCK: {
+      name: "Mrs. Peacock", location: E_ROOMS.HALLWAY8, taken: false
+  },
+  GREEN: {
+      name: "Reverend Green", location: E_ROOMS.HALLWAY11, taken: false
+  },
+  PLUM: {
+      name: "Professor Plum", location: E_ROOMS.HALLWAY3, taken: false
+  },
+  SCARLET: {
+      name: "Miss Scarlet", location: E_ROOMS.HALLWAY2, taken: false
+  },
+  WHITE: {
+      name: "Mrs. White", location: E_ROOMS.HALLWAY12, taken: false
+  }
+}
+
+const E_CARDS = {
+  COLONEL: {
+      name: E_CHARACTERS.COLONEL.name, type: "character", number: 1
+  },
+  PEACOCK: {
+      name: E_CHARACTERS.PEACOCK.name, type: "character", number: 2
+  },
+  GREEN: {
+      name: E_CHARACTERS.GREEN.name, type: "character", number: 3
+  },
+  PLUM: {
+      name: E_CHARACTERS.PLUM.name, type: "character", number: 4
+  },
+  SCARLET: {
+      name: E_CHARACTERS.SCARLET.name, type: "character", number: 5
+  },
+  WHITE: {
+      name: E_CHARACTERS.WHITE.name, type: "character", number: 6
+  },
+  STUDY: {
+      name: E_ROOMS.STUDY.name, type: "room", number: 1
+  },
+  HALL: {
+      name: E_ROOMS.HALL.name, type: "room", number: 2
+  },
+  LOUNGE: {
+      name: E_ROOMS.LOUNGE.name, type: "room", number: 3
+  },
+  LIBRARY: {
+      name: E_ROOMS.LIBRARY.name, type: "room", number: 4
+  },
+  BILLIARDROOM: {
+      name: E_ROOMS.BILLIARDROOM.name, type: "room", number: 5
+  },
+  DININGROOM: {
+      name: E_ROOMS.DININGROOM.name, type: "room", number: 6
+  },
+  CONSERVATORY: {
+      name: E_ROOMS.CONSERVATORY.name, type: "room", number: 7
+  },
+  BALLROOM: {
+      name: E_ROOMS.BALLROOM.name, type: "room", number: 8
+  },
+  KITCHEN: {
+      name: E_ROOMS.KITCHEN.name, type: "room", number: 9
+  },
+  ROPE: { 
+      name: E_WEAPONS.ROPE, type: "weapon", number: 1
+  },
+  PIPE: {
+      name: E_WEAPONS.PIPE, type: "weapon", number: 2
+  },
+  KNIFE: {
+      name: E_WEAPONS.KNIFE, type: "weapon", number: 3
+  },
+  WRENCH: {
+      name: E_WEAPONS.WRENCH, type: "weapon", number: 4
+  },
+  CANDLESTICK: {
+      name: E_WEAPONS.CANDLESTICK, type: "weapon", number: 5
+  },
+  REVOLVER: {
+      name: E_WEAPONS.REVOLVER, type: "weapon", number: 6
+  }
+}
+
+const E_TURNACTIONS = {
+  MOVE: "move",
+  ACCUSE: "accuse",
+  REFUTE: "refute"
+}
+
 const express = require("express");
 const http = require("http");
 const socketIO = require("socket.io");
@@ -10,9 +305,22 @@ const uri = process.env.DB_URI;
 
 const router = require('./router/router');
 
+playerClientMap = new Map();
+cardAllocation = new Map
+serverCharacters = null;
+dealTo = 0;
+turnOrder = new Array;
+currentTurn = 0;
+currentTurnAction = E_TURNACTIONS.MOVE;
+
+murderer = "";
+murderLocation = "";
+murderWeapon = "";
+
 // serve static build files to clients
 app.use(router);
 
+/*Database functions are currently unused
 //Update game started state on database
 async function updateGameStarted(newState){
   const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -58,31 +366,203 @@ async function retrieveGameState(){
       await client.close();
   }
 }
+*/
+
+//Increment turn order to next character, then return that character
+function getNextTurnCharacter(){
+    let nextTurn = currentTurn + 1;
+    if(nextTurn >= turnOrder.length){
+      currentTurn = 0;
+      return turnOrder[0];
+    }
+    else{
+      currentTurn = nextTurn;
+      return turnOrder[nextTurn];
+    }
+}
+
+//Return next turn action (currently only returns "move")
+function getNextTurnAction(){
+
+    //TODO: Implement suggestions/accusations
+
+    return E_TURNACTIONS.MOVE;
+}
+
+function cardSetup(){
+   let charCardIndices = [1, 2, 3, 4, 5, 6];
+   let roomCardIndices = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+   let weaponCardIndices = [1, 2, 3, 4, 5, 6];
+   charCardIndices = shuffle(charCardIndices);
+   roomCardIndices = shuffle(roomCardIndices);
+   weaponCardIndices = shuffle(weaponCardIndices);
+   for(let item in E_CARDS)
+   {
+      if(E_CARDS[item].number === charCardIndices[0] && E_CARDS[item].type === "character")
+      {
+        murderer = item;
+      }
+      if(E_CARDS[item].number === roomCardIndices[0] && E_CARDS[item].type === "room")
+      {
+        murderLocation = item;
+      }
+      if(E_CARDS[item].number === weaponCardIndices[0] && E_CARDS[item].type === "weapon")
+      {
+        murderWeapon = item;
+      }
+   }
+   for(let item in E_CARDS)
+   {
+      for(x = 1; x <= 8; x++)
+      {
+        if(E_CARDS[item].number === charCardIndices[x] && E_CARDS[item].type === "character")
+        {
+          dealCardToNextPlayer(item);
+        }
+        if(E_CARDS[item].number === roomCardIndices[x] && E_CARDS[item].type === "room")
+        {
+          dealCardToNextPlayer(item);
+        }
+        if(E_CARDS[item].number === weaponCardIndices[x] && E_CARDS[item].type === "weapon")
+        {
+          dealCardToNextPlayer(item);
+        }
+      }
+   }
+}
+
+function dealCardToNextPlayer(cardName)
+{
+  io.sockets.emit("dealCard", E_CARDS[cardName], turnOrder[dealTo])
+  let nextDeal = dealTo + 1;
+  if(nextDeal >= turnOrder.length){
+    dealTo = 0;
+  }
+  else{
+    dealTo = nextDeal;
+  }
+}
+
+//Implement Fisher-Yates shuffle
+function shuffle(array) {
+  var copy = [], n = array.length, i;
+  while (n){
+    i = Math.floor(Math.random() * n--);
+    copy.push(array.splice(i, 1)[0]);
+  }
+  return copy;
+}
 
 // This creates our socket using the instance of the server
 io.on("connection", socket => {
-    console.log("New client connected" + socket.id);
+    console.log("New client connected " + socket.id);
 
     // initial_data can be used to send state to new clients
-    socket.on("initial_data", () => {
+    socket.on("initialData", (state) => {
       console.log("Initial state");
-      retrieveGameState().then(
-        state => {io.sockets.emit("get_data", state)});
+
+      // If server state isn't set yet, initialize serverCharacters to match client (kind of hacky, can remove after database is incorporated)
+      if(serverCharacters == null)
+      {
+        serverCharacters = state.characters;
+      }
+      io.sockets.emit("updateCharacters", serverCharacters)
+      io.sockets.emit("turnChange", turnOrder[currentTurn], currentTurnAction);
+
+      //TODO: Add database retrieval 
+
+      //retrieveGameState().then(
+      //  state => {io.sockets.emit("get_data", state)});
     })
 
-    // change_game_state is called when start game/reset state button is pressed
-    socket.on("change_game_state", (started) => {
-      console.log("Change state");
-      updateGameStarted(started).catch(console.error);
-      io.sockets.emit("get_started", started);
+    socket.on("startGame", (state) => {
+      console.log("Start game pressed");
+      cardSetup();
+    })
+
+    // playerMove is called when someone moves
+    socket.on("playerMove", (characters) => {
+      console.log("Player moved");
+      serverCharacters = characters;
+
+      // TODO: Add database update
+
+      io.sockets.emit("updateCharacters", serverCharacters);
+      io.sockets.emit("turnChange", getNextTurnCharacter(), getNextTurnAction());
     });
 
-    // change_message is called when "send message" button is pressed
-    socket.on("change_message", (message) => {
-      console.log("Change message");
-      updateMessage(message).catch(console.error);
-      io.sockets.emit("get_message", message);
-    })
+    // linkPlayerWithClient is called when someone selects a character
+    socket.on("linkPlayerWithClient", (newPlayer) => {
+      console.log("Character selected");
+      
+      // TODO: Add database updates
+
+      //Set new character to taken
+      serverCharacters.find((takenCharacter, index) => {
+        if (takenCharacter.name === newPlayer) {
+          const character = serverCharacters[index];
+          character.taken = true;
+          serverCharacters[index] = character;
+          turnOrder.push(character);
+          return true;
+        }
+        return false;
+      });
+      
+      //Set old character to not taken (if there was one)
+      serverCharacters.find((freeCharacter, index) => {
+        if (freeCharacter.name === playerClientMap.get(socket.id)) {
+          const character = serverCharacters[index];
+          character.taken = false;
+          serverCharacters[index] = character;
+          turnOrder.splice(turnOrder.indexOf(character),1);
+          return true;
+        }
+        return false;
+      });
+
+      //Output turn order for debugging, and update player-client mappping
+      turnOrder.forEach(function(item) {console.log("Turn order: " + item.name)});
+
+      playerClientMap.delete(socket.id);
+      playerClientMap.set(socket.id, newPlayer);
+      io.sockets.emit("updateCharacters", serverCharacters);
+      io.sockets.emit("turnChange", turnOrder[currentTurn], currentTurnAction);
+    });
+
+    // disconnect is called when someone disconnects, freeing up their character
+    socket.on('disconnect', () => {
+      console.log("Client disconnected " + socket.id);
+      
+      // TODO: Add database updates
+
+      //If disconnecting client had active turn, change turn
+      if(turnOrder.length > 0 && turnOrder[currentTurn].name === playerClientMap.get(socket.id))
+      {
+        io.sockets.emit("turnChange", getNextTurnCharacter(), getNextTurnAction());
+      }
+
+      //Set character to not taken from disconnected client, using player-client mapping
+      if(serverCharacters != null)
+      {
+        serverCharacters.find((freeCharacter, index) => {
+          if (freeCharacter.name === playerClientMap.get(socket.id)) {
+            const character = serverCharacters[index];
+            character.taken = false;
+            serverCharacters[index] = character;
+            turnOrder.splice(turnOrder.indexOf(character),1);
+            return true;
+          }
+          return false;
+        });
+        
+        //Output turn order for debugging, and update player-client mappping
+        turnOrder.forEach(function(item) {console.log("Turn order: " + item.name)});
+        
+        io.sockets.emit("updateCharacters", serverCharacters);
+        playerClientMap.delete(socket.id);
+      }
+    });
   });
 
 // our localhost port - I think we need an env file to deploy
@@ -90,3 +570,4 @@ const port = process.env.PORT || 3001;
 
 // setup listener on port 3001
 server.listen(port, () => console.log(`Listening on port ${port}`))
+
